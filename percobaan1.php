35b2314f33753d43b04b6117b5bb6759ab26e56f
AvantikaWulandara <148681360+AvantikaWulandara@users.noreply.github.com>
2023-10-22 16:32:55 +0700

    Add files via upload

---
+++ percobaan1.php
@@ -0,0 +1,13 @@
+<?php
+$nim = "701220341";
+$nama = "avantika wulandara";
+$umur = 18;
+$nilai = 75.75;
+
+echo "Nim  : " . $nim . "</br>";
+echo "Nama : $nama</br>";
+print "Umur : ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf("Nilai : %.3f</br>", $nilai);
+
+?>
---
+++ percobaan6.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Modul 2 - Latihan 10</title>
+    <style>
+        .kotak {
+
+        }
+        .clear {
+
+        }
+    </style>
+</head>
+<body>
+    // petunjuk :
+    // .kotak dan .clear adalah kelas dari tag div
+    // tulis script PHP di dalam tag body
+    </body>
+    </html>
+    
+
+
+        
